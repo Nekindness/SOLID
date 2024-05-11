fc68ebe8674acb2967e844c237fd25dcb0092709
Nekindness <137820039+Nekindness@users.noreply.github.com>
2024-05-11 15:42:20 +0300

    I.js

--- SOLID/I.js
+++ SOLID/I.js
@@ -6,21 +6,25 @@
 
 // Класс Printer определяет интерфейс для печати
 
+// Пример, демонстрирующий принцип разделения интерфейсов
+// Классы Printer, Scanner и FaxMachine демонстрируют принцип разделения интерфейсов
+
+//В этом примере классы AllInOnePrinterScanner и AllInOnePrinterScannerFaxMachine наследуются только от тех классов, функциональность которых им нужна.
+// Это позволяет избежать заставления клиента реализовывать интерфейсы, которые не имеют к нему отношения.
+
+// Класс Printer определяет интерфейс для печати
+
 class Printer {
     print() {
         // Метод для печати
     }
 }
 
-// Класс Scanner определяет интерфейс для сканирования
-
 class Scanner {
     scan() {
         // Метод для сканирования
     }
 }
-
-// Класс FaxMachine определяет интерфейс для отправки факса
 
 class FaxMachine {
     fax() {
@@ -29,13 +33,34 @@ class FaxMachine {
 }
 
 // Класс AllInOnePrinterScanner реализует методы принтера и сканера
+class AllInOnePrinterScanner {
+    constructor() {
+        this.printer = new Printer();
+        this.scanner = new Scanner();
+    }
 
-class AllInOnePrinterScanner extends Printer, Scanner {
-    // Класс, реализующий методы принтера и сканера
+    print() {
+        this.printer.print();
+    }
+
+    scan() {
+        this.scanner.scan();
+    }
 }
 
 // Класс AllInOnePrinterScannerFaxMachine реализует методы принтера, сканера и факса
+class AllInOnePrinterScannerFaxMachine {
+    constructor() {
+        this.printer = new Printer();
+        this.scanner = new Scanner();
+        this.faxMachine = new FaxMachine();
+    }
 
-class AllInOnePrinterScannerFaxMachine extends Printer, Scanner, FaxMachine {
-    // Класс, реализующий методы принтера, сканера и факса
+    print() {
+        this.printer.print();
+    }
+
+    scan() {
+        this.scanner.scan();
+    }
 }
